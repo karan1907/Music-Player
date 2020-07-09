aea2ebbd956949faeab794ee70f0410335ead7c7
Goldy King <goldyking@Goldys-MacBook-Pro.local>
2020-07-09 10:31:35 +0530

    Added Functionality to change the song to next and previous

--- js/script.js
+++ js/script.js
@@ -1,7 +1,34 @@
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
+const image = document.querySelector("img");
 const music = document.querySelector("audio");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
+
+// Music
+const songs = [
+  {
+    name: "jacinto-1",
+    displayName: "Electric Chill Machine",
+    artist: "Jacinto Design"
+  },
+  {
+    name: "jacinto-2",
+    displayName: "Seven Nation Army (Remix)",
+    artist: "Jacinto Design"
+  },
+  {
+    name: "jacinto-3",
+    displayName: "Goodnight, Disco Queen",
+    artist: "Jacinto Design"
+  },
+  {
+    name: "metric-1",
+    displayName: "Front Row (Remix)",
+    artist: "Metric/Jacinto Design"
+  }
+];
 
 // Check if playing
 let isPlaying = false;
@@ -23,3 +50,41 @@ const pauseSong = () => {
 
 // Play/Pause Event Listener
 playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+
+// Update Dom
+const loadSong = song => {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `../music/${song.name}.mp3`;
+  image.src = `../img/${song.name}.jpg`;
+};
+
+// Current Song
+let songIndex = 0;
+
+// Previous Song
+const prevSong = () => {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+};
+
+// Next Song
+const nextSong = () => {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+};
+
+// on Load select first song
+loadSong(songs[songIndex]);
+
+// Event Listeners for buttons
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
